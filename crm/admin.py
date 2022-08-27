bb36c1dbb3a2ef301b5e8b676592466d46dd56d5
Kiash254 <codewithkiash@gmail.com>
2022-08-27 19:45:46 +0300

    added some models

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Customers,Products,Order
 # Register your models here.
+
+admin.site.site_header = "CRM Admin"
+admin.site.site_title = "CRM Admin Portal"
+admin.site.index_title = "Welcome to CRM Admin Portal"
+
+admin.site.register(Customers)
+admin.site.register(Products)
+admin.site.register(Order)
+
--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customers(models.Model):
+    name = models.CharField(max_length=200,blank=True,null=True)
+    email = models.CharField(max_length=200,blank=True,null=True)
+    phone = models.CharField(max_length=200,blank=True,null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    CATEGORY = (
+        ('Indoor','Indoor'),
+        ('Out Door','Out Door'),
+    )
+    name=models.CharField(max_length=200,blank=True,null=True)
+    price=models.FloatField(blank=True,null=True)
+    category=models.CharField(max_length=200,blank=True,null=True,choices=CATEGORY)
+    description=models.CharField(max_length=200,blank=True,null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Out for delivery','Out for delivery'),
+        ('Delivered','Delivered'),
+    )
+    customer=models.ForeignKey(Customers,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Products,null=True,on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=200,blank=True,null=True,choices=STATUS)
